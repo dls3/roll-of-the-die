64ac25abbfa29888c5546432ade71e403282358b
Doug <doug.steinberg@gmail.com>
2017-05-15 16:54:15 -0400

    Dice (pairs) complete

---
+++ dice.rb
@@ -0,0 +1,15 @@
+# Dice usually come in pairs.
+# Create a program called dice.rb that simulates two die rolls, displays the result of each,
+# and the total.
+
+rolls = []
+
+(1..2).each do |num|
+  dice = Random.rand(6) + 1
+  rolls.push(dice)
+end
+
+total = rolls[0] + rolls[1]
+
+puts "Your rolled #{rolls[0]} and #{rolls[1]}"
+puts "Your total is #{total}"
