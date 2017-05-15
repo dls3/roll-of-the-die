5eba16b23a854139eb0de30604d20d55fe65b1c7
Doug <doug.steinberg@gmail.com>
2017-05-15 16:37:13 -0400

    rolls_sorted completed

--- rolls_sorted.rb
+++ rolls_sorted.rb
@@ -1,7 +1,15 @@
 # Modify rolls_sorted.rb to store each result in an array and then
 # use the Array#sort! method to sort the results before displaying them.
 
+rolls = []
+
 (1..10).each do |num|
   dice = Random.rand(6) + 1
-  puts "The result of your roll is #{dice}"
+  # puts "The result of your roll is #{dice}"
+  rolls.push(dice)
+end
+
+rolls.sort!
+rolls.each do |roll|
+  puts "The result of your roll is #{roll}"
 end
