df6e8ba77d42c20405ccf55826c7dce93da3a31c
Doug <doug.steinberg@gmail.com>
2017-05-18 12:34:42 -0400

    How many exercise complete

---
+++ how_many.rb
@@ -0,0 +1,12 @@
+possible_perms = 0
+
+(1..6).each do |roll1|
+  (1..6).each do |roll2|
+    count2 = 1
+    total = roll1 + roll2
+    possible_perms +=1
+    puts "Dice Roll: #{roll1}, #{roll2} Total: #{total}"
+  end
+end
+
+puts "There are #{possible_perms} possible permutations."
